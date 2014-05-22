e58df9b7888c6c86a4efd2c247cdba1daba16542
Markus Holtermann <info@markusholtermann.eu>
2014-05-22 16:35:32 +0200

    Fixed neww/#32 -- Add support for posting through a widget

--- cosinnus_note/dashboard.py
+++ cosinnus_note/dashboard.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 from cosinnus.utils.dashboard import DashboardWidget, DashboardWidgetForm
 
 from cosinnus_note.models import Note
+from cosinnus_note.views import NoteCreateView
 
 
 class CompactNotesForm(DashboardWidgetForm):
@@ -15,41 +17,24 @@ class CompactNotesForm(DashboardWidgetForm):
         help_text="0 means unlimited", required=False)
 
 
-class CompactNotes(DashboardWidget):
-
-    app_name = 'note'
-    form_class = CompactNotesForm
-    model = Note
-    title = _('News')
-    user_model_attr = None
-    widget_name = 'compact news list'
-
-    def get_data(self):
-        count = int(self.config['amount'])
-        qs = self.get_queryset().all()
-
-        if count != 0:
-            qs = qs[:count]
-        data = {
-            'notes': qs,
-            'no_data': _('No news'),
-        }
-        return render_to_string('cosinnus_note/widgets/compact_news.html', data)
-
-
 class DetailedNotesForm(DashboardWidgetForm):
     amount = forms.IntegerField(label="Amount", initial=3, min_value=0,
         help_text="0 means unlimited", required=False)
 
 
-class DetailedNotes(DashboardWidget):
+class NewNoteForm(forms.ModelForm):
+
+    class Meta:
+        fields = ('title', 'text')
+        model = Note
+
+
+class BaseNotesWidget(DashboardWidget):
 
     app_name = 'note'
-    form_class = DetailedNotesForm
     model = Note
-    title = _('Detailed News')
+    template_name = None
     user_model_attr = None
-    widget_name = 'detailed news list'
 
     def get_data(self):
         count = int(self.config['amount'])
@@ -57,8 +42,41 @@ class DetailedNotes(DashboardWidget):
 
         if count != 0:
             qs = qs[:count]
+
         data = {
             'notes': qs,
             'no_data': _('No news'),
+            'widget_id': self.id,
         }
-        return render_to_string('cosinnus_note/widgets/detailed_news.html', data)
+
+        # TODO: Refactor to use the ajax views. This would
+        #       allow proper error handling.
+        if self.config.group and self.request.user.is_authenticated():
+            if self.request.method == "POST":
+                view = NoteCreateView.as_view()
+                slug = self.config.group.slug
+                response = view(self.request, group=slug)
+                return {}
+            else:
+                data['form'] = NewNoteForm()
+        return render_to_string(self.get_template_name(), data,
+                                context_instance=RequestContext(self.request))
+
+    def get_template_name(self):
+        if self.template_name is None:
+            raise ImproperlyConfigured("No template_name given")
+        return self.template_name
+
+
+class CompactNotes(BaseNotesWidget):
+    form_class = CompactNotesForm
+    template_name = 'cosinnus_note/widgets/compact_news.html'
+    title = _('News')
+    widget_name = 'compact news list'
+
+
+class DetailedNotes(BaseNotesWidget):
+    form_class = DetailedNotesForm
+    template_name = 'cosinnus_note/widgets/detailed_news.html'
+    title = _('Detailed News')
+    widget_name = 'detailed news list'
