2f80974eb695e9d4f8e789f948807962e28c5cdf
Sascha Narr <saschanarr@googlemail.com>
2014-09-01 15:30:44 +0200

    Changes for standalone item lists

--- cosinnus_note/utils/renderer.py
+++ cosinnus_note/utils/renderer.py
@@ -7,12 +7,17 @@ Created on 08.07.2014
 from __future__ import unicode_literals
 
 from cosinnus.utils.renderer import BaseRenderer
+from cosinnus_note.models import Note
 
 
 class NoteRenderer(BaseRenderer):
+    
+    model = Note
 
     template = 'cosinnus_note/attached_notes.html'
     template_single = 'cosinnus_note/single_note.html'
+    template_list = 'cosinnus_note/note_list_standalone.html'
+    
 
     @classmethod
     def render(cls, context, myobjs):
