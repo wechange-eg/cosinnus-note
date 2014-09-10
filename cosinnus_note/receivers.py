a46b9f15202ae81d7b0eefeff6ebbb5e4842ff09
Sascha Narr <saschanarr@googlemail.com>
2014-09-10 19:25:10 +0200

    Added signal and receiver to send a mail to users if one of their notes has received a comment

---
+++ cosinnus_note/receivers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from cosinnus.conf import settings
+from django.dispatch.dispatcher import receiver
+from django.contrib.auth import get_user_model
+from cosinnus.core.receivers import _get_common_mail_context, send_mail_or_fail
+from cosinnus_note.signals import note_comment_posted
+
+
+@receiver(note_comment_posted)
+def send_note_comment_mail(sender, group, user, note, comment, **kwargs):
+    context = _get_common_mail_context(sender.request, group=group, user=user)
+    creator = note.creator
+    context.update({
+        'note': note,
+        'note_text': note.text,
+        'comment_text': comment.text,
+        'creator_name': creator.get_full_name() or creator.get_username(),
+    })
+    subject = _('%(group_name)s: %(user_name)s has commented on one of your news posts on %(site_name)s!')
+    send_mail_or_fail(creator.email, subject % context, 'cosinnus_note/mail/note_comment_posted.html', context)
+                
+                
+
--- cosinnus_note/views.py
+++ cosinnus_note/views.py
@@ -22,6 +22,7 @@ from cosinnus_note.models import Note, Comment
 from django.contrib import messages
 from cosinnus.views.mixins.filters import CosinnusFilterMixin
 from cosinnus_note.filters import NoteFilter
+from cosinnus_note import signals
 
 
 class NoteCreateView(RequireWriteMixin, FilterGroupMixin, GroupFormKwargsMixin,
@@ -133,6 +134,8 @@ class CommentCreateView(RequireWriteMixin, FilterGroupMixin, CreateView):
         form.instance.creator = self.request.user
         form.instance.note = self.note
         messages.success(self.request, self.message_success)
+        if not self.note.creator == self.request.user:
+            signals.note_comment_posted.send(sender=self, group=self.group, user=self.request.user, note=self.note, comment=form.instance)
         return super(CommentCreateView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
