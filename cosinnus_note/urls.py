2b67c6fbdb6732276e58bd2181372eee46c456eb
Sascha Narr <saschanarr@googlemail.com>
2018-09-26 18:58:39 +0200

    django1.9 prep - URLs and imports

--- cosinnus_note/urls.py
+++ cosinnus_note/urls.py
@@ -1,68 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from cosinnus_note import views
 
-cosinnus_root_patterns = patterns('cosinnus_note.views', 
-    url(r'^notes/embed/all/$',
-        'note_embed_global',
-        name='embed-global'),
-                                  
-    url(r'^notes/embed/$',
-        'note_embed_current_portal',
-        name='embed-current-portal'),
-)
+cosinnus_root_patterns = [ 
+    url(r'^notes/embed/all/$', views.note_embed_global, name='embed-global'),
+    url(r'^notes/embed/$', views.note_embed_current_portal, name='embed-current-portal'),
+]
     
-cosinnus_group_patterns = patterns('cosinnus_note.views',
-    url(r'^$',
-        'note_index',
-        name='index'),
-
-    url(r'^list/$',
-        'note_list',
-        name='list'),
-                                   
-    url(r'^embed/$',
-        'note_embed',
-        name='embed'),
-
-    #url(r'^list/(?P<tag>[^/]+)/$',
-    #    'note_list',
-    #    name='list-filtered'),
-
-    url(r'^add/$',
-        'note_create',
-        name='add'),
-
-    url(r'^(?P<slug>[^/]+)/$',
-        'note_detail',
-        name='note'),
-
-    url(r'^(?P<slug>[^/]+)/delete/$',
-        'note_delete',
-        name='delete'),
-
-    url(r'^(?P<slug>[^/]+)/update/$',
-        'note_update',
-        name='update'),
-
-    url(r'^(?P<note_slug>[^/]+)/comment/$',
-        'comment_create',
-        name='comment'),
-
-    url(r'^comment/(?P<pk>\d+)/$',
-        'comment_detail',
-        name='comment-detail'),
-
-    url(r'^comment/(?P<pk>\d+)/delete/$',
-        'comment_delete',
-        name='comment-delete'),
-
-    url(r'^comment/(?P<pk>\d+)/update/$',
-        'comment_update',
-        name='comment-update'),
-                                   
-)
-
+cosinnus_group_patterns = [
+    url(r'^$', views.note_index, name='index'),
+    url(r'^list/$', views.note_list, name='list'),
+    url(r'^embed/$', views.note_embed, name='embed'),
+    url(r'^add/$', views.note_create, name='add'),
+    url(r'^(?P<slug>[^/]+)/$', views.note_detail, name='note'),
+    url(r'^(?P<slug>[^/]+)/delete/$', views.note_delete, name='delete'),
+    url(r'^(?P<slug>[^/]+)/update/$', views.note_update, name='update'),
+    url(r'^(?P<note_slug>[^/]+)/comment/$', views.comment_create, name='comment'),
+    url(r'^comment/(?P<pk>\d+)/$', views.comment_detail, name='comment-detail'),
+    url(r'^comment/(?P<pk>\d+)/delete/$', views.comment_delete, name='comment-delete'),
+    url(r'^comment/(?P<pk>\d+)/update/$', views.comment_update, name='comment-update'),
+]
 
 urlpatterns = cosinnus_group_patterns + cosinnus_root_patterns
