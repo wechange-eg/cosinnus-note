83e809941e09ceb421243406cb215ba9d68e7722
sascha <sn@sinnwerkstatt.com>
2018-11-14 18:42:32 +0100

    Django 2.0 compatibility

--- cosinnus_note/views.py
+++ cosinnus_note/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
