69cfdc736ef6291ddf8c937a95d1dd772231fdce
Sascha Narr <saschanarr@googlemail.com>
2021-02-17 13:28:15 +0100

    add-django-reverse-admin-for-taggable-object-admins

--- cosinnus_note/admin.py
+++ cosinnus_note/admin.py
@@ -2,11 +2,11 @@
 from django.contrib import admin
 
 from cosinnus_note.models import Note
-from cosinnus.admin import BaseTaggableAdminMixin
+from cosinnus.admin import BaseTaggableAdmin
 
 
-class NoteModelAdmin(BaseTaggableAdminMixin, admin.ModelAdmin):
-    list_display = BaseTaggableAdminMixin.list_display + ['short_text', ]
-    search_fields = BaseTaggableAdminMixin.search_fields + ['text', ]
+class NoteModelAdmin(BaseTaggableAdmin):
+    list_display = BaseTaggableAdmin.list_display + ['short_text', ]
+    search_fields = BaseTaggableAdmin.search_fields + ['text', ]
     
 admin.site.register(Note, NoteModelAdmin)
